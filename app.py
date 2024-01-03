e8fe2707b6299cbbd622fc7434a5c78ccf53cf34
Griffin <griffinracey@gmail.com>
2024-01-02 20:55:22 -0600

    flask app updated

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, jsonify, render_template
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, defer
 from sqlalchemy import create_engine, func
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 # DATABASE SETUP
 
@@ -32,34 +33,25 @@ def index():
     return (
         "Welcome to our Spotify dashboard!<br/><br/>"
         "Available Routes:<br/><br/>"
-        "<a href='/api/v1.0/country'>/api/v1.0/country</a><br/>"
-        "<a href='/api/v1.0/correlations'>/api/v1.0/correlations</a><br/>"
-        "<a href='/api/v1.0/map'>/api/v1.0/map</a><br/>"
+        "<a href='/test'>/test</a><br/>"
         "<a href='/api/v1.0/test'>/api/v1.0/test</a>"
     )
 
     # return render_template("index.html") 
 
-@app.route("/api/v1.0/country")
-def country():
-    return render_template("dropdownstarter.html") 
+@app.route("/test")
+def test():
+    return render_template("leafletmapzoom.html") 
 
-@app.route("/api/v1.0/correlations")
-def correlations():
-    return render_template("buttontestlong.html")
-
-@app.route("/api/v1.0/map")
-def map():
-    return render_template("leafletmapzoom.html")
 
 @app.route("/api/v1.0/test")
-def test():
+def test_data():
 
     # Create a session
     session = Session(engine)
 
     # Query the database (replace this with your actual query)
-    results = session.query(songs.trackname, songs.artistname, songs.country, songs.popularity, songs.danceability, songs.energy, songs.tempo, songs.positiveness).limit(50)
+    results = session.query(songs.trackname, songs.artistname, songs.country, songs.albumname, songs.danceability, songs.duration, songs.energy, songs.instrumentalness, songs.liveness, songs.loudness, songs.tempo, songs.positiveness)
 
     result_list = [dict(row) for row in results]
 
