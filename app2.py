c2d7fe95f601901848addfb8607c299a944a53bd
luke <lrpayne13@gmail.com>
2024-01-02 22:56:20 -0600

    Luke

--- app2.py
+++ app2.py
@@ -1,17 +1,17 @@
-# import flask and other dependencies
 from flask import Flask, jsonify, render_template
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, defer
 from sqlalchemy import create_engine, func
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 # DATABASE SETUP
 
 # create engine 
-engine = create_engine("sqlite:///data.sqlite")
+engine = create_engine("sqlite:///music.db")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -27,42 +27,30 @@ app = Flask(__name__)
 
 # Define what to do when a user hits the index route
 @app.route("/")
-def home():
+def index():
     print("Server received request for 'Home' page...")
     return (
         "Welcome to our Spotify dashboard!<br/><br/>"
         "Available Routes:<br/><br/>"
-        "<a href='/api/v1.0/country'>/api/v1.0/country</a><br/>"
-        "<a href='/api/v1.0/correlations'>/api/v1.0/correlations</a><br/>"
-        "<a href='/api/v1.0/map'>/api/v1.0/map</a><br/>"
+        "<a href='/test'>/test</a><br/>"
         "<a href='/api/v1.0/test'>/api/v1.0/test</a>"
     )
 
-    # return render_template("home.html") 
+    # return render_template("index.html") 
 
-@app.route("/api/v1.0/country")
-def country():
-    return "there will be a dropdown for you to select a country - then the most popular tracks from this country wil appear"
-    # return render_template("country.html") 
+@app.route("/test")
+def test():
+    return render_template("dropdownstarter.html") 
 
-@app.route("/api/v1.0/correlations")
-def correlations():
-    return "scatter plot and regression line for your choice of two variables"
-    # return render_template("correlations.html")
-
-@app.route("/api/v1.0/map")
-def map():
-    return "map showing the top songs for the selected country"
-    # return render_template("map.html")
 
 @app.route("/api/v1.0/test")
-def test():
+def test_data():
 
     # Create a session
     session = Session(engine)
 
     # Query the database (replace this with your actual query)
-    results = session.query(songs.TrackName, songs.ArtistName)
+    results = session.query(songs.trackname, songs.artistname, songs.country, songs.albumname, songs.danceability, songs.duration, songs.energy, songs.instrumentalness, songs.liveness, songs.loudness, songs.tempo, songs.positiveness)
 
     result_list = [dict(row) for row in results]
 
--- static/js/startup_markers.js
+++ static/js/startup_markers.js
@@ -1,34 +1,77 @@
-let myMap = L.map("map", {
-    center: [-32.8, 117.9],
-    zoom: 7
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
-  let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/15-Mapping-Web/Water_Hydrant_WCORP_070_WA_GDA2020_Public.geojson";
-  
-  d3.json(url).then(function(response) {
-  
-    //console.log(response);plotly leafy
-    features = response.features;
-  
-    //console.log(features);
-  
-    // Comment this line in to render all 80,000 markers
-    // let marker_limit = features.length;
-    let marker_limit = 1000;
-  
-    for (let i = 0; i < marker_limit; i++) {
-  
-      let location = features[i].geometry;
-      if(location){
-        L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
-      }
-  
+
+//   // Fetch the data from your API
+//   d3.json("/api/v1.0/test").then(data => {
+//     console.log(data)
+//     // Filter data for the specific artist selected by the user
+//     var filteredData = data.filter(function(d) {
+//       return d['Artist Name'] == selectedValue;
+//     });
+
+//     // Map the filtered data to plotData for Plotly
+//     var plotData = [{
+//       type: 'choropleth',
+//       locationmode: 'country names',
+//       locations: filteredData.map(d => d['Country']),
+//       z: filteredData.map(d => d['Popularity']),
+//       text: filteredData.map(d => d['Country']),
+//       autocolorscale: true
+//     }];
+
+//     var layout = {
+//       title: 'Popularity by Country for ' + selectedValue,
+//       geo: {
+//         projection: {
+//           type: 'equirectangular'
+//         }
+//       }
+//     };
+
+//     // Render the choropleth map with Plotly
+//     Plotly.newPlot('myDiv', plotData, layout);
+//   });
+// }
+function optionChanged(selectedValue) {
+  console.log("Selected artist:", selectedValue);
+
+  d3.json("/api/v1.0/test").then(function(rows) {
+
+    console.log(rows);
+    // Helper function to unpack data
+    function unpack(rows, key) {
+      return rows.map(function(row) { return row[key]; });
     }
-  
+
+    // Filter data for the specific artist selected by the user
+    var filteredData = rows.filter(function(d) {
+      return d['Artist Name'] === selectedValue;
+    });
+
+    // Prepare data for the choropleth map
+    var data = [{
+      type: 'choropleth',
+      locationmode: 'ISO-3',
+      locations: unpack(filteredData, 'country'), // Use unpack on filtered data
+      z: unpack(filteredData, 'danceability'),
+      text: unpack(filteredData, 'country'),
+      autocolorscale: true
+    }];
+
+    // Define the layout for the Plotly plot
+    var layout = {
+      title: 'Popularity by Country for ' + selectedValue,
+      geo: {
+        projection: {
+          type: 'robinson'
+        }
+      }
+    };
+
+    // Plot the data using Plotly
+    Plotly.newPlot("myDiv", data, layout, {showLink: false});
+
+  }).catch(function(err) {
+    // Error handling
+    console.error(err);
   });
-  
+}
+
