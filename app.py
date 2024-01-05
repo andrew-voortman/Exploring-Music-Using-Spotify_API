819ee8d6871b8deff347d942c9471b8539e3c3a7
Griffin <griffinracey@gmail.com>
2024-01-04 20:29:32 -0600

    added luke's/damien's visualization's

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sqlalchemy import create_engine, func
 
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
 # DATABASE SETUP
 
@@ -36,19 +37,61 @@ def index():
 
 @app.route("/api/v1.0/test")
 def test_data():
-
     # Create a session
     session = Session(engine)
 
-    # Query the database for necessary data
-    results = session.query(songs.country, songs.trackname, songs.artistname, songs.albumname, songs.danceability, songs.popularity, songs.duration, songs.energy, songs.instrumentalness, songs.liveness, songs.loudness, songs.tempo, songs.positiveness)
-    
-    result_list = [dict(row) for row in results]
+    # Query the database
+    results = session.query(
+        songs.trackname,
+        songs.artistname,
+        songs.country,
+        songs.albumname,
+        songs.danceability,
+        songs.duration,
+        songs.energy,
+        songs.instrumentalness,
+        songs.liveness,
+        songs.loudness,
+        songs.tempo,
+        songs.positiveness,
+        songs.popularity
+    )
+
+    # Convert the query result into a list of dictionaries
+    result_list = []
+    for row in results:
+        result_dict = {
+            "trackname": row[0],
+            "artistname": row[1],
+            "country": row[2],
+            "albumname": row[3],
+            "danceability": row[4],
+            "duration": row[5],
+            "energy": row[6],
+            "instrumentalness": row[7],
+            "liveness": row[8],
+            "loudness": row[9],
+            "tempo": row[10],
+            "positiveness": row[11],
+            "popularity": row[12],
+
+        }
+        result_list.append(result_dict)
 
     # Close the session
     session.close()
-    
-    return jsonify(result_list)
+
+    # Convert the list of dictionaries to a DataFrame
+    df = pd.DataFrame(result_list)
+
+    # Group by 'country' and 'artistname' and calculate the mean
+    grouped_df = df.groupby(['country', 'artistname'], as_index=False).mean(numeric_only=True)
+
+    # Convert the grouped DataFrame to a list of dictionaries
+    grouped_result_list = grouped_df.to_dict(orient='records')
+
+    # Return the aggregated data as a JSON response
+    return jsonify(grouped_result_list)
 
 @app.route("/api/v1.0/test2")
 def test_data_2():
